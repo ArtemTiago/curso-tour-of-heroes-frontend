273846061c2cf4d774d2560d948d0471bebdc561
ArtemTiago <tiagoartem@gmail.com>
2023-07-19 18:15:02 -0300

    aula 10

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Quality of the Code
+
+[![DeepScan grade](https://deepscan.io/api/teams/21733/projects/25139/branches/781180/badge/grade.svg)](https://deepscan.io/dashboard#view=project&tid=21733&pid=25139&bid=781180)
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit{
-
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Wolverine'
   };
 
-  constructor(){}
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  ngOnInit(): void{
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
+  @HostListener('keydown', ['$event'])
+  onSelectKeyDown(event: KeyboardEvent, hero: Hero): void {
+    if (event.key === 'Enter' || event.key === ' ') {
+      this.onSelect(hero);
+    }
+  }
 }
-
-
-
