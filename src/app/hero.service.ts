ab678fb43fce4c3041576c6e80896b7452bda1fe
ArtemTiago <tiagoartem@gmail.com>
2023-07-20 17:16:07 -0300

    Navegation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,5 +17,9 @@ export class HeroService {
     return heroes;
   }
 
-
+    getHero(id: number): Observable<Hero> {
+      const hero = HEROES.find(hero => hero.id === id)!;
+      this.messageService.add(`HeroService: fetched hero id=%{id}`);
+      return of(hero);
+}
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,4 @@
-import { MessageService } from './../message.service';
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
 import { HeroService } from '../hero.service'
 
@@ -11,9 +10,9 @@ import { HeroService } from '../hero.service'
 export class HeroesComponent implements OnInit {
 
  heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService ){}
+
+  constructor(private heroService: HeroService ){}
 
   ngOnInit(): void {
       this.getHeroes();
@@ -23,15 +22,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}` )
-  }
 
-  @HostListener('keydown', ['$event'])
-  onSelectKeyDown(event: KeyboardEvent, hero: Hero): void {
-    if (event.key === 'Enter' || event.key === ' ') {
-      this.onSelect(hero);
-    }
-  }
 }
